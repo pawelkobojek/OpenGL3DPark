d0861987e6e390cbc5dc449dc0e19dddeaac7179
Paweł Kobojek <PawelKobojek@gmail.com>
2015-10-23 21:51:16 +0200

    Deallocating buffers

--- GK3d/main.cpp
+++ GK3d/main.cpp
@@ -154,6 +154,9 @@ int main(int argc, const char * argv[]) {
         glfwSwapBuffers(window);
     }
     
+    glDeleteBuffers(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     
     return 0;
